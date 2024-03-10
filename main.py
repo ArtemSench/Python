635593f6b05d497e17a10fb1cf78db51ea1f351e
ArtemSench <161152828+ArtemSench@users.noreply.github.com>
2024-03-10 17:05:45 +0300

    Add files via upload
    домашняя работа по уроку "Модули и пакеты"

---
+++ main.py
@@ -0,0 +1,4 @@
+import two_def
+print(dir(two_def))
+two_def.hello_f()
+two_def.hello_n('Denis')
---
+++ two_def.py
@@ -0,0 +1,4 @@
+def hello_f():
+    print('Hello, world!')
+def hello_n(name):
+    print('Hello, ' + name + '!')
