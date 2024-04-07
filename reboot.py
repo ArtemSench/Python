3d4e8d83de8bfe2c0333af8622a9089ba64373fc
ArtemSench <161152828+ArtemSench@users.noreply.github.com>
2024-04-07 12:35:12 +0300

    reboot.py

--- reboot.py
+++ reboot.py
@@ -1,23 +1,33 @@
 class Building:
     def __init__(self):
-        self.numberOfFloors = 12
-        self.buildingType = 'Многоквартирный'
+        self.numberOfFloors = []
+        self.buildingType = []
+    def __str__(self):
+        return 'Тип здания: ', self.buildingType
+    def __int__(self):
+        return 'Этажность: ', self.numberOfFloors
     def __eq__(self, other):
-        return self.numberOfFloors == other.numberOfFloors
-        self.buildingType == other.buildingType
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
 
-def comparison():
-    if Building.__eq__(self=Home, other=House):
-        print('Дом, милый дом!')
-    else:
-        print('Похож, но не наш дом')
+home = Building()
+house = Building()
+home.buildingType = 'Многоквартирный'
+home.numberOfFloors = 5
+house.buildingType = 'Многоквартирный'
+house.numberOfFloors = 12
 
-Home = Building()
-House = Building()
-Home.buildingType = 'Многоквартирный'
-Home.numberOfFloors = 5
-comparison()
-House.numberOfFloors = 5
-comparison()
+if home == house:
+    print('Дом, милый дом!')
+else:
+    print('Похож, но не наш дом')
+
+house.numberOfFloors = 5
+
+if home == house:
+    print('Дом, милый дом!')
+else:
+    print('Похож, но не наш дом')
+
+
 
 
