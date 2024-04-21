ce083a11af3cc787311412b63a3c5bfc5cd46094
ArtemSench <161152828+ArtemSench@users.noreply.github.com>
2024-04-21 15:56:09 +0300

    Add files via upload

---
+++ File_os.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+direct = 'c:\\tom\\files'
+for root, dirs, files in os.walk(direct):
+    for file in files:
+        filepath = os.path.join(root,file)
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime('%d.%m.%Y %H:%M', time.localtime(filetime))
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(filepath)
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, \n'
+              f'Время изменения: {formatted_time}, Родительская директория: {parent_dir}\n')
+        print(f'*' * 24)
