b97a783e54eeefbe691d111732485595b8533b2d
ArtemSench <161152828+ArtemSench@users.noreply.github.com>
2024-05-04 19:25:01 +0300

    Update HW_9-3.py

--- HW_9-3.py
+++ HW_9-3.py
@@ -1,19 +1,18 @@
 class EvenNumbers:
 
     def __init__(self, start, end):
-        self.i, self.start, self.end = 0, start, end
+        self.i, self.start, self.end = start - 1, start, end
     def __iter__(self):
-        self.i = 0
+        self.i = self.start - 1
         return self
     def __next__(self):
-        self.i = self.start - 1
         self.i += 1
-        if self.i > self.end:
-            raise StopIteration
         if self.i % 2 == 0:
+            if self.i > self.end:
+                raise StopIteration
             return self.i
         if self.i % 2 == 1:
-            self.i +=1
+            return ''
 
 
 en = EvenNumbers(10, 25)
