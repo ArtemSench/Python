a9e86e3e0fc5dc0a65a6ed04605c1f896fc21d2e
ArtemSench <161152828+ArtemSench@users.noreply.github.com>
2024-05-28 14:08:54 +0300

    HW 10-1.py

--- HW 10-1.py
+++ HW 10-1.py
@@ -8,9 +8,9 @@ def count(lst):
 lst1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 lst2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
-thread = Thread(target=count, kwargs=dict(lst=lst1))
-thread.start()
-
-count(lst=lst2)
-
-thread.join()
+thread1 = Thread(target=count, kwargs=dict(lst=lst1))
+thread2 = Thread(target=count, kwargs=dict(lst=lst2))
+thread1.start()
+thread2.start()
+thread1.join()
+thread2.join()
