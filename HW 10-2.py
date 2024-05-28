2da71c03b84d550fd2ce6fda925f4a17e7c8dae2
ArtemSench <161152828+ArtemSench@users.noreply.github.com>
2024-05-28 15:26:51 +0300

    HW 10-2.py

--- HW 10-2.py
+++ HW 10-2.py
@@ -1,14 +1,27 @@
 from threading import Thread
+import time
+from collections import defaultdict
 class Knight(Thread):
 
-    def __init__(self, name, skill, *args, **kwargs):
-        super.__init__(*args, **kwargs)
+    def __init__(self, name, skill, warriors, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.name = name
+        self.warriors = warriors
         self.skill = skill
-    def attack
+        self.kill = defaultdict(int)
 
-knight1 = Knight("Sir Lancelot", 10)  # Низкий уровень умения
-knight2 = Knight("Sir Galahad", 20)  # Высокий уровень умения
+    def run(self):
+        self.kill = defaultdict(int)
+        self.dayfight = self.warriors//self.skill
+        print(f'{self.name}: на нас напали!')
+        for day in range(1, self.dayfight):
+            print(f'{self.name}: сражается {day} день(дня)..., осталось {self.warriors - self.skill} воинов.')
+            self.warriors = self.warriors - self.skill
+        print(f'{self.name}одержал победу спустя {self.dayfight} дней!')
+        time.sleep(1)
+
+knight1 = Knight("Sir Lancelot", 10, 100)  # Низкий уровень умения
+knight2 = Knight("Sir Galahad", 20, 100)  # Высокий уровень умения
 knight1.start()
 knight2.start()
 knight1.join()
